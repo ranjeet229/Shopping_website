0c3205cd4d8800da3a8abf6276ba9b015cdf9b00
Ranjeet <kiranjeetkr80@gmail.com>
2024-08-12 03:13:28 +0530

    first

---
+++ script.js
@@ -0,0 +1,6 @@
+// Example: Handling 'Add to Cart' button clicks
+document.querySelectorAll('.btn-primary').forEach(button => {
+    button.addEventListener('click', () => {
+        alert('Product added to cart!');
+    });
+});
